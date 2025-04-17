b4bba74d860bee9194c73f1d757dc7e570738d6b
luanrobert07 <luan.robert@gec.inatel.br>
2025-04-17 11:43:59 +0200

    fix: build errors

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -4,5 +4,5 @@ export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
   const metadata = paymentData.metadata;
   const userEmail = metadata?.email;
   const testeId = metadata?.testeId;
-  console.log("PAGAMENTO COM SUCESSO", paymentData);
+  console.log("PAGAMENTO COM SUCESSO", {userEmail, testeId, paymentData});
 }
